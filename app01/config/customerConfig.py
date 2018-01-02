47fb83f1a68758fc315cecb78ba16adf9a36f76f
TakeMeHiger <492745473@qq.com>
2018-01-02 15:19:10 +0800

    实现批量导入和模板下载功能

--- app01/config/customerConfig.py
+++ app01/config/customerConfig.py
@@ -7,13 +7,15 @@ from django.shortcuts import redirect, HttpResponse, render
 from django.http import QueryDict
 from django.conf.urls import url
 from django.forms import ModelForm
-
+from django.http import StreamingHttpResponse
 
 from stark.service import v1
 from app01 import models
 from stark.utils import message
-#from tiga import  Tiga as t
-#from tiga1 import  Tiga
+
+
+# from tiga import  Tiga as t
+# from tiga1 import  Tiga
 
 
 
@@ -167,14 +169,15 @@ class CustomerConfig(v1.StarkConfig):
         #     customers = models.Customer.objects.filter(con).all()
         #     print(customers, '*****')
         # 方法二:
-        current_user_id=5
+        current_user_id = 5
         now_date = datetime.datetime.now().date()
         order_deadtime = now_date - datetime.timedelta(days=15)
         talk_deadtime = now_date - datetime.timedelta(days=3)
         customers = models.Customer.objects.filter(
             Q(recv_date__lt=order_deadtime) | Q(last_consult_date__lt=talk_deadtime), status=2).all()
 
-        return render(request, 'public_customer_view.html', {"customers": customers,'current_user_id':current_user_id})
+        return render(request, 'public_customer_view.html',
+                      {"customers": customers, 'current_user_id': current_user_id})
 
     def myuser_view(self, request):
         '''
@@ -197,24 +200,24 @@ class CustomerConfig(v1.StarkConfig):
         :return:
         '''
 
-        current_usr_id = 5
+        current_usr_id = request.session['user'].get('id')
         now_date = datetime.datetime.now().date()
         order_deadtime = now_date - datetime.timedelta(days=15)
         talk_deadtime = now_date - datetime.timedelta(days=3)
         update_count = models.Customer.objects.filter(
-            Q(recv_date__lt=order_deadtime) | Q(last_consult_date__lt=talk_deadtime), status=2,id=cid).exclude(
+            Q(recv_date__lt=order_deadtime) | Q(last_consult_date__lt=talk_deadtime), status=2, id=cid).exclude(
             consultant_id=current_usr_id).update(consultant_id=current_usr_id, last_consult_date=now_date,
                                                  recv_date=now_date)
         if not update_count:
             return HttpResponse("抢单失败")
 
-        models.CustomerDistribution.objects.create(user_id=current_usr_id,customer_id=cid,ctime=now_date)
+        models.CustomerDistribution.objects.create(user_id=current_usr_id, customer_id=cid, ctime=now_date)
         return HttpResponse('抢单成功')
 
-    def singleInput_view(self,request):
-        if request.method=='GET':
-            form=SingleModelForm()
-            return render(request,'singleInput.html',{'form':form})
+    def singleInput_view(self, request):
+        if request.method == 'GET':
+            form = SingleModelForm()
+            return render(request, 'singleInput.html', {'form': form})
         else:
             """客户表新增数据：
                              - 获取该分配的课程顾问id
@@ -223,115 +226,124 @@ class CustomerConfig(v1.StarkConfig):
                              - 获取新创建的客户ID
                              - 顾问ID
                           """
-            form=SingleModelForm(data=request.POST)
+            form = SingleModelForm(data=request.POST)
             if form.is_valid():
 
-               from tiga1 import Tiga
-               user_id=Tiga.get_sale_id()
-               if not user_id:
-                   return HttpResponse('无销售顾问,无法进行分配')
-               try:
-                   with transaction.atomic():
-                       now_date = datetime.datetime.now().date()
-                       # form.cleaned_data['consultant_id'] = user_id
-                       # form.cleaned_data['status'] = 2
-                       # form.cleaned_data['last_consult_date'] = now_date
-                       # form.cleaned_data['recv_date'] = now_date
-                       # course = form.cleaned_data.pop('course')
-                       # customer = models.Customer.objects.create(**form.cleaned_data)
-                       # print(form)
-                       # customer.course.add(*course)
-                       form.instance.consultant_id=user_id
-                       form.instance.last_consult_date=now_date
-                       form.instance.recv_date=now_date
-                       customer=form.save()
-                       print(customer)
-                       print('.....')
-                       models.CustomerDistribution.objects.create(customer=customer, user_id=user_id, ctime=now_date)
-                       body='给你分配新用户了'
-                       print(564123)
-                       message.send_message('分配新用户',body,'1789920207@qq.com','zjm')
+                from tiga1 import Tiga
+                user_id = Tiga.get_sale_id()
+                if not user_id:
+                    return HttpResponse('无销售顾问,无法进行分配')
+                try:
+                    with transaction.atomic():
+                        now_date = datetime.datetime.now().date()
+                        # form.cleaned_data['consultant_id'] = user_id
+                        # form.cleaned_data['status'] = 2
+                        # form.cleaned_data['last_consult_date'] = now_date
+                        # form.cleaned_data['recv_date'] = now_date
+                        # course = form.cleaned_data.pop('course')
+                        # customer = models.Customer.objects.create(**form.cleaned_data)
+                        # print(form)
+                        # customer.course.add(*course)
+                        form.instance.consultant_id = user_id
+                        form.instance.last_consult_date = now_date
+                        form.instance.recv_date = now_date
+                        customer = form.save()
+                        print(customer)
+                        print('.....')
+                        models.CustomerDistribution.objects.create(customer=customer, user_id=user_id, ctime=now_date)
+                        body = '给你分配新用户了'
+                        print(564123)
+                        message.send_message('分配新用户', body, '1789920207@qq.com', 'zjm')
 
-               except Exception as e:
-                   #Tiga.rollback(user_id)
-                   return HttpResponse('录入异常')
+                except Exception as e:
+                    # Tiga.rollback(user_id)
+                    return HttpResponse('录入异常')
 
-
-               return redirect('/stark/app01/customer/')
+                return redirect('/stark/app01/customer/')
 
             return render(request, 'singleInput.html', {'form': form})
 
-
-    def multiInput_view(self,request):
-        if request.method=='GET':
-            return render(request,'multiInput.html')
+    def multiInput_view(self, request):
+        if request.method == 'GET':
+            return render(request, 'multiInput.html')
         else:
             from tiga1 import Tiga
-            user_id=Tiga.get_sale_id()
-            now_date=datetime.datetime.now().date()
-            fileObj=request.FILES.get('file')
-
-            with open('multiInput.xlsx','wb')as f:
-                for chuck in fileObj:
-                    f.write(chuck)
+            user_id = Tiga.get_sale_id()
+            now_date = datetime.datetime.now().date()
+            fileObj = request.FILES.get('file')
+            # with open('multiInput.xlsx', 'wb')as f:
+            #     for chuck in fileObj:
+            #         f.write(chuck)
 
             import xlrd
-            workbook=xlrd.open_workbook('multiInput.xlsx')
-            sheet=workbook.sheet_by_index(0)
-            maps={
-                0:'qq',
-                1:'name',
-                2:'gender',
-                3:'course',
+            #workbook = xlrd.open_workbook('multiInput.xlsx')
+            workbook = xlrd.open_workbook(file_contents=fileObj.read())
+            sheet = workbook.sheet_by_index(0)
+            maps = {
+                0: 'qq',
+                1: 'name',
+                2: 'gender',
+                3: 'course',
             }
 
-
-            for index in range(1,sheet.nrows):
-                row=sheet.row(index)
+            for index in range(1, sheet.nrows):
+                row = sheet.row(index)
                 dic = {}
                 for i in maps:
                     key = maps[i]
                     cell = row[i]
-                    if i in (0,2):
-                        dic[key]=int(cell.value)
-                    elif i==1:
-                        dic[key] =cell.value
+                    if i in (0, 2):
+                        dic[key] = int(cell.value)
+                    elif i == 1:
+                        dic[key] = cell.value
                     else:
-                        course_id=cell.value
-                        course_list_str=course_id.split('_')
-                        course_list_int=[]
+                        course_id = cell.value
+                        course_list_str = course_id.split('_')
+                        course_list_int = []
                         for i in course_list_str:
                             course_list_int.append(int(i))
-                        dic[key]=course_list_int
-
-
+                        dic[key] = course_list_int
 
                 print(dic)
                 try:
                     with transaction.atomic():
 
-                        dic['consultant_id']=user_id
-                        dic['recv_date']=now_date
+                        dic['consultant_id'] = user_id
+                        dic['recv_date'] = now_date
                         print(dic)
                         print(type(dic['qq']))
-                        course_list=dic.pop('course')
+                        course_list = dic.pop('course')
                         print(dic)
                         print(course_list)
-                        customer=models.Customer.objects.create(**dic)
+                        customer = models.Customer.objects.create(**dic)
                         customer.course.add(*course_list)
                         print(customer)
-                        models.CustomerDistribution.objects.create(customer=customer,user_id=user_id,ctime=now_date)
+                        models.CustomerDistribution.objects.create(customer=customer, user_id=user_id, ctime=now_date)
 
                 except Exception as e:
                     Tiga.rollback(user_id)
                     return HttpResponse('批量导入出现异常')
 
-
-
-
-
             return HttpResponse('ok')
 
+    def downtemplate_view(self, request):
+        # do something...
+
+        def file_iterator(file_name, chunk_size=512):
+            with open(file_name, 'rb') as f:
+                while True:
+                    c = f.read(chunk_size)
+                    if c:
+                        yield c
+                    else:
+                        break
+
+        the_file_name = "模板.xlsx"
+        response = StreamingHttpResponse(file_iterator(the_file_name))
+        response['Content-Type'] = 'application/octet-stream'
+        response['Content-Disposition'] = 'attachment;filename="{0}"'.format(the_file_name)
+
+        return response
 
     def extra_url(self):
         app_model_class = self.model_class._meta.app_label, self.model_class._meta.model_name
@@ -342,5 +354,6 @@ class CustomerConfig(v1.StarkConfig):
             url(r'^(\d+)/competition/$', self.wrap(self.competition_view), name="%s_%s_competition" % app_model_class),
             url(r'^singleInput/$', self.wrap(self.singleInput_view), name='%s_%s_singleInput' % app_model_class),
             url(r'^multiInput/$', self.wrap(self.multiInput_view), name='%s_%s_multiInput' % app_model_class),
+            url(r'^downtemplate/$', self.wrap(self.downtemplate_view), name='%s_%s_downtemplate' % app_model_class),
         ]
         return patterns
